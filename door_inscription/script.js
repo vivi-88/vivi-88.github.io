a5d85a8d03f51c73d238576f03372515fe2c42c0
vivi88 <vivi88@github.com>
2022-11-15 17:46:06 +0100

    secret message: second attempt

--- door_inscription/script.js
+++ door_inscription/script.js
@@ -1,37 +1,38 @@
-var canvas = document.getElementById('tilt');
-var ctx = canvas.getContext('2d');
 
-canvas.width = 800;
-canvas.height = 600;
-ctx.font = '25px Bradley Hand ITC';
-ctx.textAlign = 'center';
-ctx.textBaseline = 'middle';
 
-var scale = {
-  h: canvas.height/20,
-  v: canvas.width/20
-};
+
 var text = {
   h: 
-  'I.,L.,V.: Abbiamo rispolverato questo vecchio trucco,\n' + 
-  'spero vi torni in mente. Siamo tornati da poco, re Oberon\n' + 
-  'sembra aver perso il senno. Diffidate delle guardie cittadine,' + 
-  'e non fatevi trovare allo scoperto. La città non è sicura, ci',
+  "I.,L.,V.: Abbiamo rispolverato questo vecchio trucco,\n" + 
+  "spero vi torni in mente. Siamo tornati da poco, re Oberon\n" + 
+  "sembra aver perso il senno. Diffidate delle guardie cittadine,\n" + 
+  "e non fatevi trovare allo scoperto. La città non è sicura, ci",
   v: 
-  "siamo rifugiati all'esterno -- il richiamo delle armi certo vi" + 
-  "condurrà al nostro rifugio. Avanzate attraverso le rovine:" + 
-  "sono pericolanti, ma danno protezione. E non c'è rischio che" + 
-  "il cielo vi caschi sulla testa per nostra mano. A presto."
+  "siamo rifugiati all'esterno -- il richiamo delle armi certo vi\n" + 
+  "condurrà al nostro rifugio. Avanzate attraverso le rovine:\n" + 
+  "sono pericolanti, ma danno protezione. E non c'è rischio che\n" + 
+  "il cielo vi caschi sulla testa per nostra mano. A presto.\n"
 };
 
+function update(canvas, text) {
+	
+  var ctx = canvas.getContext('2d');
+  var scale = {
+    h: canvas.height/4,
+	v: canvas.width/4
+  };
 
-
-function update() {
-    ctx.fillStyle = "#fff";
+  canvas.width = 600;
+  canvas.height = 600;
+  ctx.font = '25px Bradley Hand ITC';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  
+  ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   
-    ctx.fillStyle = "#000";
-    ctx.font = '25px Bradley Hand ITC';
+  ctx.fillStyle = "#000";
+  ctx.font = '25px Bradley Hand ITC';
     
   ctx.save();
   ctx.scale(1, scale.h);
@@ -47,4 +48,4 @@ function update() {
   ctx.restore();
 }
 
-update();
+update(document.getElementById('tilt'), {h: 'I.,L.,V.: Abbiamo rispolverato questo vecchio trucco,', v: 'siamo rifugiati all\'esterno - il richiamo delle armi certo vi'});
